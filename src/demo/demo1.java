22626d9cb3bd704f60f81b10a296407c103ec195
aa <aa>
2017-08-22 10:26:02 +0800

    asd

---
+++ src/demo/demo1.java
@@ -0,0 +1,10 @@
+package demo;
+
+/**
+ * Created by Administrator on 2017/8/22 0022.
+ */
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("9999");
+    }
+}
